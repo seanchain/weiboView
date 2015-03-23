052df55a218e3b2d9abfee16b72ed874e234b998
Sean Chain <seanchain2.0@gmail.com>
2015-03-23 12:31:53 +0800

    small modification

--- 微博/ViewController.m
+++ 微博/ViewController.m
@@ -59,8 +59,8 @@ NSIndexPath *idxpth;
         NSMutableArray *models = [NSMutableArray arrayWithCapacity:dictArray.count];
         
         
-        //NSURL *url = [NSURL URLWithString:@"http://chensihang.com/blog/?json=1"];
-        NSString *str = [NSString stringWithContentsOfFile:@"/Users/seanchain/Desktop/index.txt" encoding:NSUTF8StringEncoding error:nil];
+        NSURL *url = [NSURL URLWithString:@"http://chensihang.com/blog/?json=1"];
+        NSString *str = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
         NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
         NSArray *arr = [json objectForKey:@"posts"];
@@ -101,7 +101,6 @@ NSIndexPath *idxpth;
             [dictarrs addObject:tempdic];
         }
         
-        dictarrs[0][@"picture"] = @"http://localhost:8888/pic/profile.jpg";
         NSLog(@"%@", dictarrs);
 
         
